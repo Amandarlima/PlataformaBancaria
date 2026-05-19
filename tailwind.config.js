19e7cee9d978d854467adf47a8047d4cac31b315
Amanda Ribas Lima <amandaribaslima@hotmail.com>
2026-05-19 00:58:17 -0300

    chore: inicia estrutura React + TypeScript + Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        bank: {
+          50: "#ecf7ff",
+          100: "#d3edff",
+          500: "#0066b3",
+          700: "#004a82",
+          900: "#0b2437"
+        }
+      }
+    }
+  },
+  plugins: []
+};
